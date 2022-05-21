6288ea19b718761a5b305e421767482b0933a7e3
Dan Frumin <dan@groupoid.moe>
2022-05-21 20:52:25 +0200

    working on linearizing terms

--- theories/terms.v
+++ theories/terms.v
@@ -175,6 +175,7 @@ Proof.
     set_solver by lia.
 Qed.
 
+
 Lemma linearize_pre_linear T idx :
   linear_bterm (snd $ linearize_pre T idx).
 Proof.
@@ -245,80 +246,207 @@ Proof.
   all: by rewrite IHT1 IHT2.
 Qed.
 
+(***************************************************)
+(** TODO: move this out *)
+(** Code by Blaisorblade  *)
+From stdpp Require Import finite functions.
+Class FiniteDomain {A B M} `{Lookup A B (M B)} (m : M B) := {
+  enum_domain : list A;
+  finite_mapping (a : A) (b : B) :
+    m !! a = Some b -> a ∈ enum_domain;
+}.
+Arguments enum_domain {A B M _} m {_} : assert.
+Section fin_domain.
+  Context `{EqDecision B}.
+  Context {A M} (m : M B) `{FiniteDomain A B M m}.
+  Definition preimage (b : B) : list A :=
+    filter (λ a, m !! a = Some b) (enum_domain m).
+  Context `{!EqDecision A, !Countable A}.
+  Definition preimage_set (b : B) : gset A := list_to_set (preimage b).
 
-Definition linearize_bterm_ren_act {PROP : bi} T Xs : nat → PROP := λ (n : nat), Xs (linearize_bterm_ren T !!! n).
-Lemma linearize_bterm_act {PROP : bi} (T : bterm nat) (Xs : nat → PROP) :
-  bterm_alg_act (linearize_bterm T) (linearize_bterm_ren_act T Xs) ≡ bterm_alg_act T Xs.
-Proof.
-  rewrite /linearize_bterm_ren_act /linearize_bterm_ren /linearize_bterm.
-  generalize 0.
-  induction T=>i; simpl.
-  - by rewrite lookup_total_singleton.
-  - specialize (IHT1 i).
-    destruct (linearize_pre T1 i) as [[i1 m1] T1'] eqn:Ht1.
-    specialize (IHT2 i1).
-    destruct (linearize_pre T2 i1) as [[i2 m2] T2'] eqn:Ht2.
-    assert (term_fv T1' ⊆ set_seq i (i1-i)).
-    { replace T1' with (snd (linearize_pre T1 i)) by rewrite Ht1 //.
-      replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
-      apply linearize_pre_fv. }
-    assert (term_fv T2' ⊆ set_seq i1 (i2-i1)).
-    { replace T2' with (snd (linearize_pre T2 i1)) by rewrite Ht2 //.
-      replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
-      apply linearize_pre_fv. }
-    assert (dom (gset _) m1 = set_seq i (i1-i)) as Hm1.
-    { replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
-      replace m1 with (snd $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
-      apply linearize_pre_dom. }
-    assert (dom (gset _) m2 = set_seq i1 (i2-i1)) as Hm2.
-    { replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
-      replace m2 with (snd $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
-      apply linearize_pre_dom. }
-    simpl. repeat split; eauto.
-    rewrite -IHT1 -IHT2. f_equiv.
-    + eapply bterm_alg_act_fv.
-      intros j Hj.
-      assert ((m1 ∪ m2) !!! j = m1 !!! j) as ->; eauto.
-      unfold lookup_total, map_lookup_total. f_equiv.
-      apply lookup_union_l, elem_of_dom. rewrite Hm1. set_solver by lia.
-    + eapply bterm_alg_act_fv.
-      intros j Hj.
-      assert ((m1 ∪ m2) !!! j = m2 !!! j) as ->; eauto.
-      unfold lookup_total, map_lookup_total. f_equiv.
-      apply lookup_union_r. apply not_elem_of_dom. rewrite Hm1. set_solver by lia.
-  - specialize (IHT1 i).
-    destruct (linearize_pre T1 i) as [[i1 m1] T1'] eqn:Ht1.
-    specialize (IHT2 i1).
-    destruct (linearize_pre T2 i1) as [[i2 m2] T2'] eqn:Ht2.
-    assert (term_fv T1' ⊆ set_seq i (i1-i)).
-    { replace T1' with (snd (linearize_pre T1 i)) by rewrite Ht1 //.
-      replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
-      apply linearize_pre_fv. }
-    assert (term_fv T2' ⊆ set_seq i1 (i2-i1)).
-    { replace T2' with (snd (linearize_pre T2 i1)) by rewrite Ht2 //.
-      replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
-      apply linearize_pre_fv. }
-    assert (dom (gset _) m1 = set_seq i (i1-i)) as Hm1.
-    { replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
-      replace m1 with (snd $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
-      apply linearize_pre_dom. }
-    assert (dom (gset _) m2 = set_seq i1 (i2-i1)) as Hm2.
-    { replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
-      replace m2 with (snd $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
-      apply linearize_pre_dom. }
-    simpl. repeat split; eauto.
-    rewrite -IHT1 -IHT2. f_equiv.
-    + eapply bterm_alg_act_fv.
-      intros j Hj.
-      assert ((m1 ∪ m2) !!! j = m1 !!! j) as ->; eauto.
-      unfold lookup_total, map_lookup_total. f_equiv.
-      apply lookup_union_l, elem_of_dom. rewrite Hm1. set_solver by lia.
-    + eapply bterm_alg_act_fv.
-      intros j Hj.
-      assert ((m1 ∪ m2) !!! j = m2 !!! j) as ->; eauto.
-      unfold lookup_total, map_lookup_total. f_equiv.
-      apply lookup_union_r. apply not_elem_of_dom. rewrite Hm1. set_solver by lia.
-Qed.
+  Lemma preimage_eq_1 a b :
+    b ∈ preimage a → m !! b = Some a.
+  Proof. rewrite /preimage elem_of_list_filter. naive_solver. Qed.
+
+  Lemma preimage_eq_2 a b :
+    m !! b = Some a → b ∈ preimage a.
+  Proof.
+    rewrite /preimage elem_of_list_filter. intros ?; split; auto.
+    by eapply finite_mapping.
+  Qed.
+
+  Lemma preimage_eq a b :
+    b ∈ preimage a ↔ m !! b = Some a.
+  Proof. split; [ apply preimage_eq_1 | apply preimage_eq_2 ]. Qed.
+
+  Lemma preimage_set_eq a b :
+    b ∈ preimage_set a ↔ m !! b = Some a.
+  Proof.
+    rewrite /preimage_set elem_of_list_to_set.
+    apply preimage_eq.
+  Qed.
+End fin_domain.
+
+Section finite_preimage.
+  Context `{!EqDecision A, !Finite A} {B : Type} (f : A -> B).
+  #[local] Set Default Proof Using "Type*".
+
+  (* Instance fn_lookup_total : LookupTotal A B (A -> B) := λ a f, f a. *)
+  Instance fn_lookup : Lookup A B (A -> B) := λ a f, Some (f a).
+  #[refine] Global Instance finite_finitedomain : FiniteDomain (M := λ B, A -> B) f :=
+  { enum_domain := enum _ }.
+  Proof. intros; apply elem_of_enum. Qed.
+End finite_preimage.
+
+Section map_preimage.
+  Context {K A : Type} `{FinMap K M}.
+  Context `{!EqDecision A, !Countable A, !EqDecision K, !Countable K}.
+  Variable (m : M A).
+
+  #[refine] Global Instance finmapmap_finitedomain : FiniteDomain m :=
+  { enum_domain := fst <$> map_to_list m }.
+  (* XXX fix set_solver. *)
+  Proof. move=> k a /elem_of_map_to_list. apply: elem_of_list_fmap_1. Qed.
+
+End map_preimage.
+
+Section preimage_properties.
+  Context `{!EqDecision A, !Countable A, !EqDecision K, !Countable K}.
+
+  Lemma preimage_set_singleton i x : preimage_set ({[ i := x ]} : gmap K A) x ≡ {[i]}.
+  Proof.
+    intros z. rewrite preimage_set_eq.
+    rewrite lookup_singleton_Some. set_solver.
+  Qed.
+
+End preimage_properties.
+(** / END TODO *)
+
+Section linearize_bterm_properties.
+  Context {PROP : bi}.
+  Variable (T : bterm nat).
+  Implicit Type Xs Ys : nat → PROP.
+  Implicit Type n k m : nat.
+
+  Definition linearize_bterm_ren_act Xs : nat → PROP :=
+    λ (n : nat), Xs (linearize_bterm_ren T !!! n).
+
+  Definition ren_inverse : nat → gset nat := preimage_set (linearize_bterm_ren T).
+
+  Definition mk_or Xs j (P : PROP) : PROP := (Xs j ∨ P)%I.
+  Local Instance mk_or_proper Xs : Proper ((=) ==> (≡) ==> (≡)) (mk_or Xs).
+  Proof. Admitted.
+
+  Definition set_disj Xs (s : gset nat) := set_fold (mk_or Xs) False%I s.
+  Local Instance set_disj_proper Xs : Proper ((≡) ==> (≡)) (set_disj Xs).
+  Proof. Admitted.
+  Definition disj_ren_inverse Xs : nat → PROP := λ n, set_disj Xs (ren_inverse n).
+
+  Lemma linearize_bterm_act Xs :
+    bterm_alg_act (linearize_bterm T) Xs ≡ bterm_alg_act T (disj_ren_inverse Xs).
+  Proof.
+    rewrite /disj_ren_inverse /ren_inverse /linearize_bterm_ren /linearize_bterm.
+    generalize 0.
+    induction T as [x | T1 IHT1 T2 IHT2 | T1 IHT1 T2 IHT2 ]=>i; simpl.
+    - rewrite preimage_set_singleton.
+      rewrite /set_disj set_fold_singleton /mk_or. by rewrite right_id.
+    - specialize (IHT1 i).
+      destruct (linearize_pre T1 i) as [[i1 m1] T1'] eqn:Ht1.
+      specialize (IHT2 i1).
+      destruct (linearize_pre T2 i1) as [[i2 m2] T2'] eqn:Ht2. simpl.
+      assert (dom (gset _) m1 = set_seq i (i1-i)) as Hm1.
+      { replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        replace m1 with (snd $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        apply linearize_pre_dom. }
+      assert (dom (gset _) m2 = set_seq i1 (i2-i1)) as Hm2.
+      { replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        replace m2 with (snd $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        apply linearize_pre_dom. }
+      assert (m1 ##ₘ m2) as Hm1m2disj.
+      { apply map_disjoint_dom_2. rewrite Hm1 Hm2.
+        set_unfold. lia. }
+      f_equiv.
+      { rewrite IHT1 /=. apply bterm_alg_act_fv.
+        intros j Hj. f_equiv. Abort.
+
+  Lemma linearize_bterm_act_ren Xs:
+    bterm_alg_act (linearize_bterm T) (linearize_bterm_ren_act Xs) ≡ bterm_alg_act T Xs.
+  Proof.
+    rewrite /linearize_bterm_ren_act /linearize_bterm_ren /linearize_bterm.
+    generalize 0.
+    induction T as [x | T1 IHT1 T2 IHT2 | T1 IHT1 T2 IHT2 ]=>i; simpl.
+    - by rewrite lookup_total_singleton.
+    - specialize (IHT1 i).
+      destruct (linearize_pre T1 i) as [[i1 m1] T1'] eqn:Ht1.
+      specialize (IHT2 i1).
+      destruct (linearize_pre T2 i1) as [[i2 m2] T2'] eqn:Ht2.
+      assert (term_fv T1' ⊆ set_seq i (i1-i)).
+      { replace T1' with (snd (linearize_pre T1 i)) by rewrite Ht1 //.
+        replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        apply linearize_pre_fv. }
+      assert (term_fv T2' ⊆ set_seq i1 (i2-i1)).
+      { replace T2' with (snd (linearize_pre T2 i1)) by rewrite Ht2 //.
+        replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        apply linearize_pre_fv. }
+      assert (dom (gset _) m1 = set_seq i (i1-i)) as Hm1.
+      { replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        replace m1 with (snd $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        apply linearize_pre_dom. }
+      assert (dom (gset _) m2 = set_seq i1 (i2-i1)) as Hm2.
+      { replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        replace m2 with (snd $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        apply linearize_pre_dom. }
+      simpl. repeat split; eauto.
+      rewrite -IHT1 -IHT2. f_equiv.
+      + eapply bterm_alg_act_fv.
+        intros j Hj.
+        assert ((m1 ∪ m2) !!! j = m1 !!! j) as ->; eauto.
+        unfold lookup_total, map_lookup_total. f_equiv.
+        apply lookup_union_l, elem_of_dom. rewrite Hm1. set_solver by lia.
+      + eapply bterm_alg_act_fv.
+        intros j Hj.
+        assert ((m1 ∪ m2) !!! j = m2 !!! j) as ->; eauto.
+        unfold lookup_total, map_lookup_total. f_equiv.
+        apply lookup_union_r. apply not_elem_of_dom. rewrite Hm1. set_solver by lia.
+    - specialize (IHT1 i).
+      destruct (linearize_pre T1 i) as [[i1 m1] T1'] eqn:Ht1.
+      specialize (IHT2 i1).
+      destruct (linearize_pre T2 i1) as [[i2 m2] T2'] eqn:Ht2.
+      assert (term_fv T1' ⊆ set_seq i (i1-i)).
+      { replace T1' with (snd (linearize_pre T1 i)) by rewrite Ht1 //.
+        replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        apply linearize_pre_fv. }
+      assert (term_fv T2' ⊆ set_seq i1 (i2-i1)).
+      { replace T2' with (snd (linearize_pre T2 i1)) by rewrite Ht2 //.
+        replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        apply linearize_pre_fv. }
+      assert (dom (gset _) m1 = set_seq i (i1-i)) as Hm1.
+      { replace i1 with (fst $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        replace m1 with (snd $ fst (linearize_pre T1 i)) by rewrite Ht1 //.
+        apply linearize_pre_dom. }
+      assert (dom (gset _) m2 = set_seq i1 (i2-i1)) as Hm2.
+      { replace i2 with (fst $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        replace m2 with (snd $ fst (linearize_pre T2 i1)) by rewrite Ht2 //.
+        apply linearize_pre_dom. }
+      simpl. repeat split; eauto.
+      rewrite -IHT1 -IHT2. f_equiv.
+      + eapply bterm_alg_act_fv.
+        intros j Hj.
+        assert ((m1 ∪ m2) !!! j = m1 !!! j) as ->; eauto.
+        unfold lookup_total, map_lookup_total. f_equiv.
+        apply lookup_union_l, elem_of_dom. rewrite Hm1. set_solver by lia.
+      + eapply bterm_alg_act_fv.
+        intros j Hj.
+        assert ((m1 ∪ m2) !!! j = m2 !!! j) as ->; eauto.
+        unfold lookup_total, map_lookup_total. f_equiv.
+        apply lookup_union_r. apply not_elem_of_dom. rewrite Hm1. set_solver by lia.
+  Qed.
+
+End linearize_bterm_properties.
+
+(* Definition t0 := (TSemic (TComma (Var 0) (Var 0)) (Var 1)). *)
+(* Eval compute in t0. *)
+(* Eval compute in (linearize_bterm t0). *)
 
 (** Interpretation of bunches is a homomorphism w.r.t. bunched terms:
 <<
