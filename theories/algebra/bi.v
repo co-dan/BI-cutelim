369198aa2bd971870845da6db585333a5e292ba1
Dan Frumin <dan@groupoid.moe>
2023-07-14 11:30:56 +0200

    update to work with the latest coq version

--- theories/algebra/bi.v
+++ theories/algebra/bi.v
@@ -4,8 +4,8 @@
 From bunched.algebra Require Export interface derived_laws.
 From stdpp Require Import prelude.
 
-Instance Prop_equiv : Equiv Prop := iff.
-Instance Prop_equivalence : Equivalence (≡@{Prop}) := _.
+#[export] Instance Prop_equiv : Equiv Prop := iff.
+#[export] Instance Prop_equivalence : Equivalence (≡@{Prop}) := _.
 
 Module Import bi.
   Export interface.bi.
